b71c562f83cb585a88f2e780f94d79f2ca8de258
josealbertobritosantana <josealbertobritosantana@gmail.com>
2024-11-27 16:52:43 +0000

    more added to preview

--- scripts/engine.js
+++ scripts/engine.js
@@ -2,17 +2,29 @@ console.log("Script is running");
 let activeOn = false
 
 function proj_preview(){
-    exist = document.querySelector(".preview-box")
+    const exist = document.querySelector(".preview-box")
+    const main = document.getElementsByTagName("main")
     if (activeOn === true){
-        exist.remove();
+        exist.remove(); //remove the preview panel
+        document.body.style.overflow = "scroll" //stops scrolling
         activeOn = false
     }else{
         const preview_cont = document.createElement("div")
+        const img_caroussel = document.createElement("img")
+        img_caroussel.setAttribute("src", "images/placeholder.png")
+        img_caroussel.style.width = "100%"
+        img_caroussel.style.height = "auto"
+        img_caroussel.style.objectFit = "cover"
+        preview_cont.appendChild(img_caroussel)
         preview_cont.classList.add("preview-box")
-        document.body.appendChild(preview_cont)
+        main[0].appendChild(preview_cont)
+        document.body.style.overflow = "hidden"
+        
+        
+        
         activeOn = true
     }
 }
 
 let preview_call = document.querySelectorAll(".item-preview"); //take care of it later
-preview_call.addEventListener("click", proj_preview)
+preview_call.forEach((item) => addEventListener("click", proj_preview))
